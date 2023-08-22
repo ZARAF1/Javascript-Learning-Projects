17ec6e85d2e6ee33f1f63d9925ba77b78271a79f
ZARAF1 <ali.raffad@gmail.com>
2023-08-22 18:16:00 +0300

    added loop  task 2 for fundamentals part 2 assignment

--- 02-Fundamentals-Part-2/Assessment/Assignment/JavaScript Fundamentals – Part 2/script.js
+++ 02-Fundamentals-Part-2/Assessment/Assignment/JavaScript Fundamentals – Part 2/script.js
@@ -84,3 +84,15 @@ for (let i = 0; i < populationsTwo.length; i++) {
   percentageOfWorld4.push(percentageOfWorld(populationsTwo[i]));
 }
 console.log(percentageOfWorld4);
+
+// task 2
+const listOfNeighbours = [
+  ['Canada', 'Mexico'],
+  ['Spain'],
+  ['Norway', 'Sweden', 'Russia'],
+];
+for (let i = 0; i < listOfNeighbours.length; i++) {
+  for (let j = 0; j < listOfNeighbours[i].length; j++) {
+    console.log(`Neighbour: ${listOfNeighbours[i][j]}`);
+  }
+}
