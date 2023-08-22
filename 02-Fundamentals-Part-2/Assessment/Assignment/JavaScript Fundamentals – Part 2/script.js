5111211721eaa793984bfa544e34899d918635aa
ZARAF1 <ali.raffad@gmail.com>
2023-08-22 17:13:40 +0300

    added object tasks for fundamentals part 2 assignment

--- 02-Fundamentals-Part-2/Assessment/Assignment/JavaScript Fundamentals – Part 2/script.js
+++ 02-Fundamentals-Part-2/Assessment/Assignment/JavaScript Fundamentals – Part 2/script.js
@@ -48,3 +48,27 @@ if (!neighbours.includes('Germany')) {
 const indiaIndex = neighbours.indexOf('india');
 neighbours[0] = 'Jamaica';
 console.log(neighbours);
+
+/** Objects */
+const myCountry = {
+  country: 'Pakistan',
+  capital: 'Islamabad',
+  language: 'Urdu',
+  population: 23,
+  neighbours: ['india', 'iran', 'afghanistan', 'china'],
+  describe: function () {
+    console.log(
+      `${this.country} has ${this.population} million ${this.language}-speaking people. ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}!`
+    );
+  },
+  checkIsland() {
+    this.isIsland = this.neighbours.length <= 0 ? true : false;
+  },
+};
+// console.log(
+//   `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people. ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}!`
+// );
+myCountry.describe();
+myCountry.checkIsland();
+console.log(myCountry.isIsland);
+/** Loops */
